c1c2ea69fa07b841fd3b9eda456d56ea7b2df06b
Syed-Huzaifa <shahhuzaifa338@gmail.com>
2025-02-24 11:31:41 +0500

    Add PostCSS configuration and update Tailwind CSS setup; enhance styles and theme variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
